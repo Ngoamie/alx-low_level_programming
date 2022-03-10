034493d9cdd20d7f3d83f5c4c846307acb7c77b8
Ngoamie <ngomeme28@gmail.com>
2022-03-10 05:51:38 -0800

    Preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Printf
+ *
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
